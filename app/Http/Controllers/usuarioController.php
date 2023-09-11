cc84c4f7e06d7c1a52f4167afc44919ded0cba25
Alan Juaááárez <alanjuarez.n64@gmail.com>
2023-09-11 17:21:04 -0300

    se añadieron las funciones buscar, eliminar, modificar y ver todo para los usuarios

--- app/Http/Controllers/usuarioController.php
+++ app/Http/Controllers/usuarioController.php
@@ -5,41 +5,80 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\User;
 
 class usuarioController extends Controller
 {
-    public function Registrar(Request $request)
 
+    public function verTodos()
     {
-        $accessToken = $request->user()->token->accessToken;
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
+    }
 
-        $response = Http::post('http://localhost:8001/api/user', [
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
+
+
+    public function Registrar(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:6',
         ]);
 
-        return response()->json(['message' => 'Usuario registrado exitosamente']);
+        $user = User::create($data);
 
-}
+        return response()->json(['message' => 'Usuario creado con éxito', 'data' => $user], 201);
+    }
 
 
-    public function Buscar(Request $request){
 
-        $accessToken = $request->user()->token->accessToken;
-
-        $id = $request->input('id');
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $accessToken,
-        ])->get('http://localhost:8001/api/' . $id);
     
-        if ($response->successful()) {
-            $usuario = $response->json();
-            return response()->json(['usuario' => $usuario]);
-        } else {
-            return response()->json(['error' => 'Error al buscar el usuario'], 500);
+
+    public function Buscar($id)
+    {
+        $user = User::find($id);
+        
+        if (!$user) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
         }
 
+        return response()->json(['data' => $user], 200);
     }
+
+
+    public function Modificar(Request $request, $id)
+    {
+        $user = User::find($id);
+        
+        if (!$user) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        $data = $request->validate([
+            'name' => 'string',
+            'email' => 'email|unique:users,email,' . $id,
+            'password' => 'string|min:6',
+        ]);
+
+        $user->update($data);
+
+        return response()->json(['message' => 'Usuario actualizado con éxito', 'data' => $user], 200);
+    }
+
+    public function Eliminar($id)
+    {
+        $user = User::find($id);
+        
+        if (!$user) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'Usuario eliminado con éxito'], 204);
+    }
+
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+
+    public static function Buscar($terminodeBusqueda)
+    {
+        return self::where('name', 'like', "%$terminodeBusqueda%")
+            ->orWhere('email', 'like', "%$terminodeBusqueda%")
+            ->get();
+    }
 }
