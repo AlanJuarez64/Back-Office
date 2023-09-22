52527f69f7f08cbff4454bd3f5e7420f94bdd29e
Alan Juaááárez <alanjuarez.n64@gmail.com>
2023-09-22 17:02:40 -0300

    Se actualizó el login para que sea mas similar a la pagina principal del sistema

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,25 +1,37 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="{{asset('/css/style-login.css')}}">
-</head>
-<body>
-<section class="contenido-login">
-<form id="login" action="/login" method="post">
-<div>
-    <h1>Inicio de Sesión</h1>
-    @csrf
-    <input type="email" id="email" name="email" placeholder="Ingrese su email" required>
-    <input type="password" id="password" name="password" placeholder="Contraseña" required>
+<html lang="en">
 
-    <div>
-    <button id="btn-login" type="submit">Iniciar sesión</button>
-    </div>
-</div>
-</form>
-</section>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QuickHaul - Login</title>
+  <link rel="stylesheet" href="{{asset('/css/style-login.css')}}">
+  <header>
+
+  </header>
+</head>
+<br>
+<br>
+
+<body>
+  <section class="contenido-login">
+
+    <form id="form-login" action="/login" method="POST">
+        <h1>Inicio de Sesion</h1>
+      <br>
+      <br>
+      @csrf
+
+      <input type="email" id="email" name="email" placeholder="Ingrese su email" required>
+      <input type="password" id="password" name="password" placeholder="Contraseña" required>
+
+      <div>
+        <button id="btn-login" type="submit">Iniciar sesión</button>
+        <p class="advert" id="advert"></p>
+      </div>
+    </form>
+  </section>
+
+  
 </body>
+
 </html>
