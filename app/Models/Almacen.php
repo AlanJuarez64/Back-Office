c06ee827a8d381b58c3797e0fba68c28b4180d0a
Alan Juaááárez <alanjuarez.n64@gmail.com>
2023-09-26 00:05:51 -0300

    Se generó el modelo Almacen en base a la base de datos

---
+++ app/Models/Almacen.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Almacen extends Model
+{
+    use HasFactory;
+    protected $table = 'Almacenes';
+    protected $primaryKey = 'ID_Almacen';
+    public $timestamps = true;
+    protected $fillable = [
+        'Capacidad',
+    ];
+}
