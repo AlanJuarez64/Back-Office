e6d8c3a2f3c2b5571c546ba89bf2a33c2084ee6f
Alan Juaááárez <alanjuarez.n64@gmail.com>
2023-11-10 13:17:00 -0300

    Se agregaron datos al registrar y se agregó la opcion de eliminar usuario

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -20,14 +20,27 @@
         <div class="gestor-usuarios">
             <!-- Formulario para agregar usuarios -->
             <form id="agregar-usuario-form" method="post" action="/usuarios/">
-            @csrf
                 <h2>Registrar Usuario</h2>
+                @csrf
                 <label for="name" id="label-form">Nombre:</label>
                 <input type="text" id="name" name="name" required>
+                <label for="nombre_completo" id="label-form">Nombre completo:</label>
+                <input type="text" id="nombre_completo" name="nombre_completo" required>
+                <label for="ci" id="label-form">CI:</label>
+                <input type="text" id="ci" name="ci" required>
                 <label for="email" id="label-form">Correo:</label>
                 <input type="email" id="email" name="email" required>
                 <label for="password" id="label-form">Contraseña:</label>
                 <input type="password" id="password" name="password" required>
+                <br>
+                <label for="tipo_usuario" id="label-form">Tipo de Usuario:</label><br>
+                <select name="tipo_usuario" id="tipo_usuario">
+                    <option value="chofer">Chofer</option>
+                    <option value="despachador">Despachador</option>
+                    <option value="funcionario_de_transporte">Funcionario de transporte</option>
+                    <option value="funcionario_de_almacen">Funcionario de almacen</option>
+                </select>
+                <br><br>
                 <button type="submit">Registrar</button>
             </form>
             <br><br>
@@ -36,39 +49,43 @@
                 {{ session('success_message') }}
                 </div>
             @endif
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <p style ="color:red">{{ $errors }}</p>
+            </div>
+            @endif
 
             <br><br>
             <form action="/usuarios/all" method="get">
             <button id="cargar-usuarios-btn">Cargar Usuarios</button>
+            <br><br>
             <div id="lista-usuarios"> 
             </form>
             <ul>
             @isset($users)
             @foreach($users as $user)
-                ID:{{ $user->id }} - {{ $user->name }} - {{ $user->email }} <br><br>
-            @endforeach
+            <div class="bloque-usuario">
+                ID:{{ $user->id }} - {{ $user->name }} - {{ $user->email }}
+                <form action="/usuarios/{{ $user->id }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">Eliminar Usuario</button>
+                </form>
+            </div>
+                <br><br>
+                @endforeach
             @endif
             </ul>
             </div>
-            
-            <!-- Formulario para eliminar usuarios -->
-            <br><br>
-            <form id="formEliminar">
-            @csrf
-            <h2>Eliminar Usuario</h2>
-            <label for="idUser">ID del Usuario:</label>
-            <input type="number" id="idUser" name="id" required>
-            <button type="submit">Eliminar Usuario</button>
-            </form>
         </div>
     </div>
 
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script>
-        //Menú 'Toggle'
+
         let toggle = document.querySelector('.toggle');
-        let nav = document.querySelector('nav'); // Cambiado a 'nav'
+        let nav = document.querySelector('nav'); 
         let main = document.querySelector('.main');
 
         toggle.addEventListener('click', function () {
@@ -77,7 +94,6 @@
         });
 
 
-        // Añadir clase "hovered" al ítem seleccionado.
         let list = document.querySelectorAll('nav li');
 
         function activarLink(event) {
