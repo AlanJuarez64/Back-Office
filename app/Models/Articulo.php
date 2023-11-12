d76d5bcf0f3c9c8041ffb99b7c3119ef59d8d346
Alan Juaááárez <alanjuarez.n64@gmail.com>
2023-11-12 19:15:09 -0300

    se creo el modelo de articulos

---
+++ app/Models/Articulo.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Articulo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'Articulo';
+
+    protected $primaryKey = 'ID_Articulo';
+
+    protected $fillable = [
+        'ID_Usuario',
+        'ID_Producto',
+        'Estado',
+        'created_at',
+        'updated_at',
+    ];
+    
+    public function producto()
+    {
+        return $this->belongsTo(Producto::class, 'ID_Producto', 'ID_Producto');
+    }
+
+}
